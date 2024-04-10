6fcc43c3d8381889e66340f6ea68994cdafdcf6e
Nik0-9 <filannino.nicola9@gmail.com>
2024-04-10 14:47:26 +0200

    INIT scaffolding

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <HeaderComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './assets/components/HeaderComponent.vue'
+  export default {
+    name: 'App',
+    components:{
+        HeaderComponent
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
